9a6c2aa8fdd5e012ee198fb3fab750ac47660f41
Ferran Vilà Conesa <fnva@gft.com>
2015-03-17 17:31:04 +0100

    Initial version

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,114 @@
 #!/usr/bin/env node
 
-var lib= require('../lib/index.js');
-var greeting = lib.sayHello('Bret');
+'use strict';
 
-console.log(greeting);
+// Custom Dependencies
+var lib = require('../lib/index.js');
+var constants_jre = require('../node_modules/npm-adaptiveme-jre/lib/constants.js');
+var lib_jre = require('../node_modules/npm-adaptiveme-jre/lib/index.js');
+
+// Global Dependencies
+var program = require('commander');
+var colors = require('colors');
+var os = require('os');
+var async = require("async");
+var jre = require('npm-adaptiveme-jre');
+require('shelljs/global');
+
+// Define program
+program
+    .version('1.0.3')
+    .option('-a, --api <version>', 'Adaptive API level to emulate. \nDefault: 2.2.0')
+    .option('-d, --device <id>', 'Launch emulator with given device id (id retrieved from --list option). \nDefault: Will launch a cool smartphone.')
+    .option('-l, --list <type>', 'List devices for type [all, generic, wearable, smartphone, tablet, browser, desktop, television]. \nDefault: all')
+    .option('-p, --path <uri>', 'Launch emulator with given URI (if URI is file://, you can combine this with the -w option. \nDefault: file:///<localdir>/index.html')
+    .option('-w, --watch <boolean>', 'Watch local resources and reload emulator on changes.\nDefault: false')
+    .parse(process.argv);
+
+
+if (!program.path) {
+    console.log(colors.red("You have to pass the -p argument"));
+    return -1;
+}
+
+var params = " ";
+if (program.api) params = params + "-a " + program.api + " ";
+if (program.device) params = params + "-d " + program.device + " ";
+if (program.list) params = params + "-l " + program.list + " ";
+if (program.path) params = params + "-p " + program.path + " ";
+if (program.watch) params = params + "-w " + program.watch + " ";
+
+
+console.log(colors.green("Running Adaptive Nibble..."));
+
+// Set the JAVA_HOME
+var platform = lib_jre.getPlatformByNodePlatformAndArch(os.platform(), os.arch());
+
+async.series([
+        function (callback) {
+            jre.execute(callback);
+            //callback(null, '');
+        }/*,
+        function (callback) {
+
+            var java_home = "";
+            switch (platform.node_os) {
+                case "darwin":
+                    java_home = __dirname + '/../jdk' + constants_jre.jdk_version + '/jdk' + constants_jre.jdk_version + '.jdk/Contents/Home';
+                    break;
+                case "win32":
+                    java_home = __dirname + '/../jdk' + constants_jre.jdk_version + '.jdk';
+                    break;
+                case "linux":
+                    java_home = __dirname + '/../jdk' + constants_jre.jdk_version + '.jdk/' + constants_jre.jdk_version;
+                    break;
+                default:
+                    console.log(colors.red("The current platform is not supported by the system. Exiting"));
+                    return -1
+            }
+
+            process.env['JAVA_HOME'] = java_home;
+            console.log(colors.green("Setting JAVA_HOME = " + java_home));
+
+            // Running the emulator
+
+            console.log(colors.green("Running the emulator..."));
+
+            if (exec(__dirname + '/adaptive-nibble-emulator/bin/adaptive-nibble-emulator').code !== 0) {
+                console.log(colors.red("Error running the emulator. Exiting"));
+                return -1
+            }
+            callback(null, '');
+        }*/
+    ],
+    // optional callback
+    function (err, results) {
+        var java_home = "";
+        switch (platform.node_os) {
+            case "darwin":
+                java_home = __dirname + '/../jdk' + constants_jre.jdk_version + '/jdk' + constants_jre.jdk_version + '.jdk/Contents/Home';
+                break;
+            case "win32":
+                java_home = __dirname + '/../jdk' + constants_jre.jdk_version + '.jdk';
+                break;
+            case "linux":
+                java_home = __dirname + '/../jdk' + constants_jre.jdk_version + '.jdk/' + constants_jre.jdk_version;
+                break;
+            default:
+                console.log(colors.red("The current platform is not supported by the system. Exiting"));
+                return -1
+        }
+
+        process.env['JAVA_HOME'] = java_home;
+        console.log(colors.green("Setting JAVA_HOME = " + java_home));
+
+        // Running the emulator
+
+        console.log(colors.green("Running the emulator..."));
+
+        if (exec(__dirname + '/adaptive-nibble-emulator/bin/adaptive-nibble-emulator' + params).code !== 0) {
+            console.log(colors.red("Error running the emulator. Exiting"));
+            return -1
+        }
+    });
+
