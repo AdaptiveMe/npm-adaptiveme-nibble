b10177393b56f3eda88bacb6daead637f8d67626
Ferran Vilà Conesa <fnva@gft.com>
2015-03-19 11:10:41 +0100

    Version 1.0.3

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,18 @@
 
 'use strict';
 
+var program = require('commander');
+
+// Define program
+program
+    .version('1.0.3')
+    .option('-a, --api <version>', 'Adaptive API level to emulate. Default: 2.2.0')
+    .option('-d, --device <id>', 'Launch emulator with given device id (id retrieved from --list option). Default: Will launch a cool smartphone.')
+    .option('-l, --list <type>', 'List devices for type [all, generic, wearable, smartphone, tablet, browser, desktop, television]. Default: all')
+    .option('-p, --path <uri>', 'Launch emulator with given URI (if URI is file://, you can combine this with the -w option. Default: file:///<localdir>/index.html')
+    .option('-w, --watch <boolean>', 'Watch local resources and reload emulator on changes.Default: false')
+    .parse(process.argv);
+
 // Custom Dependencies
 var lib = require('../lib/index.js');
 var constants_jre = require('../node_modules/npm-adaptiveme-jre/lib/constants.js');
@@ -15,19 +27,9 @@ var async = require("async");
 var jre = require('npm-adaptiveme-jre');
 require('shelljs/global');
 
-// Define program
-program
-    .version('1.0.3')
-    .option('-a, --api <version>', 'Adaptive API level to emulate. \nDefault: 2.2.0')
-    .option('-d, --device <id>', 'Launch emulator with given device id (id retrieved from --list option). \nDefault: Will launch a cool smartphone.')
-    .option('-l, --list <type>', 'List devices for type [all, generic, wearable, smartphone, tablet, browser, desktop, television]. \nDefault: all')
-    .option('-p, --path <uri>', 'Launch emulator with given URI (if URI is file://, you can combine this with the -w option. \nDefault: file:///<localdir>/index.html')
-    .option('-w, --watch <boolean>', 'Watch local resources and reload emulator on changes.\nDefault: false')
-    .parse(process.argv);
-
 
 if (!program.path) {
-    console.log(colors.red("You have to pass the -p argument"));
+    console.log(colors.red("You have to pass the -p argument <uri> Launch emulator with given URI "));
     return -1;
 }
 
@@ -47,39 +49,7 @@ var platform = lib_jre.getPlatformByNodePlatformAndArch(os.platform(), os.arch()
 async.series([
         function (callback) {
             jre.execute(callback);
-            //callback(null, '');
-        }/*,
-        function (callback) {
-
-            var java_home = "";
-            switch (platform.node_os) {
-                case "darwin":
-                    java_home = __dirname + '/../jdk' + constants_jre.jdk_version + '/jdk' + constants_jre.jdk_version + '.jdk/Contents/Home';
-                    break;
-                case "win32":
-                    java_home = __dirname + '/../jdk' + constants_jre.jdk_version + '.jdk';
-                    break;
-                case "linux":
-                    java_home = __dirname + '/../jdk' + constants_jre.jdk_version + '.jdk/' + constants_jre.jdk_version;
-                    break;
-                default:
-                    console.log(colors.red("The current platform is not supported by the system. Exiting"));
-                    return -1
-            }
-
-            process.env['JAVA_HOME'] = java_home;
-            console.log(colors.green("Setting JAVA_HOME = " + java_home));
-
-            // Running the emulator
-
-            console.log(colors.green("Running the emulator..."));
-
-            if (exec(__dirname + '/adaptive-nibble-emulator/bin/adaptive-nibble-emulator').code !== 0) {
-                console.log(colors.red("Error running the emulator. Exiting"));
-                return -1
-            }
-            callback(null, '');
-        }*/
+        }
     ],
     // optional callback
     function (err, results) {
