16d9407f34e32f87a79f65ef257c079039e60b91
Ferran Vilà Conesa <fnva@gft.com>
2015-05-28 13:59:09 +0200

    Adaptive Nibble 1.0-alpha.11

--- bin/install.js
+++ bin/install.js
@@ -1,128 +1,132 @@
+/**
+ * This script runs the installation of the nibble software. The script
+ * checks if it's necesary to install or reinstall the nibble and
+ * downloads the nibble if it's necessari to (re)install.
+ */
+
 'use strict';
 
 var colors = require('colors/safe'),
-  trycatch = require('trycatch'),
-  exit = require('exit'),
-  lib = require('../lib/lib.js'),
-  os = require('os'),
-  async = require('async'),
-  wget = require('wget-improved'),
-  Progress = require('progress'),
-  fs = require('fs'),
-  path = require('path'),
-  tarball = require('tarball-extract'),
-  AdmZip = require('adm-zip');
+    trycatch = require('trycatch'),
+    exit = require('exit'),
+    lib = require('../lib/lib.js'),
+    async = require('async'),
+    wget = require('wget-improved'),
+    Progress = require('progress'),
+    fs = require('fs'),
+    path = require('path'),
+    tarball = require('tarball-extract'),
+    AdmZip = require('adm-zip'),
+    osenv = require('osenv');
 
 trycatch(function () {
 
-  // Current Platform
-  var platform = lib.getPlatformByNodePlatformAndArch(os.platform(), os.arch());
+    // Current Platform
+    var platform = lib.getPlatform();
 
-  if (!platform) {
-    console.log(colors.red.bold('[nibble] There is no platform configured for the current operating system: %s %s'), os.platform(), os.arch());
-    exit(-1);
-  } else {
-    console.log(colors.green('[nibble] Current platform: %s'), platform.name);
-  }
+    async.series({
 
-  async.series({
+            nibble: function (callback) {
 
-      nibble: function (callback) {
+                var isDownload = false;
+                var percentStatus = -1;
+                var adaptive_dir = osenv.home() + path.sep + '.adaptive';
+                var nibble_dir = adaptive_dir + path.sep + '.nibble';
+                var nibble_file = nibble_dir + platform.nibble_file;
 
-        var isDownload = false;
-        var percentStatus = -1;
-        var nibble_dir = path.dirname(fs.realpathSync(__dirname)) + path.sep + '..' + platform.nibble_dir;
-        var nibble_file = nibble_dir + platform.nibble_file;
+                var bar = new Progress('[:bar] :percent :elapseds :etas ', {
+                    complete: '=',
+                    incomplete: ' ',
+                    total: 101,
+                    width: 50
+                });
 
-        var bar = new Progress('[:bar] :percent :elapseds :etas ', {
-          complete: '=',
-          incomplete: ' ',
-          total: 101,
-          width: 50
-        });
+                // Check if its necessary to download the nibble
+                if (fs.existsSync(nibble_dir)) {
 
-        // Check if its necessary to download the nibble
-        if (fs.existsSync(nibble_dir)) {
-          // If the nibble directory exists, check the version
+                    // If the nibble directory exists, check the version
+                    if (fs.existsSync(nibble_file)) {
+                        // Same version
+                        console.log(colors.green('[nibble] Your current version of nibble is the latest. Skipping nibble download...'));
+                        isDownload = false;
+                    } else {
+                        // Different version
+                        console.log(colors.red('[nibble] Your current version of nibble is an old one.'));
+                        lib.deleteFolderRecursive(nibble_dir);
+                        isDownload = true;
+                    }
 
-          if (fs.existsSync(nibble_file)) {
-            // Same version
-            console.log(colors.green('[nibble] Your current version of nibble is the latest. Skipping nibble download...'));
-            isDownload = false;
-          } else {
-            // Different version
-            console.log(colors.red('[nibble] Your current version of nibble is an old one.'));
-            lib.deleteFolderRecursive(nibble_dir);
-            isDownload = true;
-          }
-
-        } else {
-          // If the nibble directory doesn't exist, download it
-          isDownload = true;
-        }
-
-        if (isDownload) {
-
-          console.log(colors.magenta('[nibble] Downloading nibble... %s'), platform.nibble_url);
-
-          fs.mkdirSync(nibble_dir);
-
-          var download = wget.download(platform.nibble_url, nibble_file, null);
-
-          download.on('error', function (err) {
-            callback(err);
-          });
-
-          download.on('end', function () {
-
-            console.log(colors.magenta('[nibble] Extracting nibble: %s'), nibble_file);
-
-            if (platform.nibble_ext === 'tgz') {
-
-              tarball.extractTarball(nibble_file, nibble_dir, function (err) {
-                if (err) {
-                  callback(err);
+                } else {
+                    // If the nibble directory doesn't exist, download it
+                    isDownload = true;
                 }
-                console.log(colors.magenta('[nibble] Succesfully extracted nibble'));
-                callback(null);
-              });
 
-            } else if (platform.nibble_ext === 'zip') {
+                if (isDownload) {
 
-              var zip = new AdmZip(nibble_file);
-              zip.extractAllTo(nibble_dir, true);
+                    console.log(colors.magenta('[nibble] Downloading nibble... %s'), platform.nibble_url);
 
-              console.log(colors.magenta('[nibble] Succesfully extracted nibble'));
-              callback(null);
+                    if (!fs.existsSync(adaptive_dir)) {
+                        fs.mkdirSync(adaptive_dir);
+                    }
 
+                    fs.mkdirSync(nibble_dir);
+
+                    var download = wget.download(platform.nibble_url, nibble_file, null);
+
+                    download.on('error', function (err) {
+                        callback(err);
+                    });
+
+                    download.on('end', function () {
+
+                        console.log(colors.magenta('[nibble] Extracting nibble: %s'), nibble_file);
+
+                        if (platform.nibble_ext === 'tgz') {
+
+                            tarball.extractTarball(nibble_file, nibble_dir, function (err) {
+                                if (err) {
+                                    callback(err);
+                                }
+                                console.log(colors.magenta('[nibble] Succesfully extracted nibble'));
+                                callback(null);
+                            });
+
+                        } else if (platform.nibble_ext === 'zip') {
+
+                            var zip = new AdmZip(nibble_file);
+                            zip.extractAllTo(nibble_dir, true);
+
+                            console.log(colors.magenta('[nibble] Succesfully extracted nibble'));
+                            callback(null);
+
+                        }
+                    });
+
+                    download.on('progress', function (progress) {
+                        var percent = (progress * 100).toFixed(0);
+                        if (percent !== percentStatus) {
+                            percentStatus = percent;
+                            bar.tick();
+                        }
+                    });
+
+                }
             }
-          });
+        },
+        // optional callback
+        function (err) {
 
-          download.on('progress', function (progress) {
-            var percent = (progress * 100).toFixed(0);
-            if (percent !== percentStatus) {
-              percentStatus = percent;
-              bar.tick();
+            if (err) {
+                console.log(colors.red.bold('[nibble] Error: %s'), err);
+                exit(-1);
+            } else {
+                exit(0);
             }
-          });
-
-        }
-      }
-    },
-    // optional callback
-    function (err) {
-
-      if (err) {
-        console.log(colors.red.bold('[nibble] Error: %s'), err);
-        exit(-1);
-      } else {
-        exit(0);
-      }
-    });
+        });
 
 
 }, function (err) {
 
-  console.log(colors.red.bold('[nibble] Error: %s'), err);
-  exit(-1);
+    console.log(colors.red.bold('[nibble] Error: %s'), err);
+    exit(-1);
 });
--- bin/version.js
+++ bin/version.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 
+/**
+ * This script can be run externally and returns the current
+ * version of the script configured in the package.json
+ */
+
 'use strict';
 
 var pJson = require('../package.json');
